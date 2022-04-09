20759d0dbf6391528ef42bba9c364f9e0760abcf
moshi <sy.41211@gmail.com>
2022-04-09 17:20:44 +0900

    Add: COG functional classification color config

--- src/csplotter/config.py
+++ src/csplotter/config.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Union
 
+from matplotlib import colors
+
 
 @dataclass
 class Arg:
@@ -11,10 +13,10 @@ class Arg:
 
 
 radius_args_dict = {
-    "forward_cds_r": Arg(0.07, "Forward CDS radius size"),
-    "reverse_cds_r": Arg(0.07, "Reverse CDS radius size"),
-    "rrna_r": Arg(0.07, "rRNA radius size"),
-    "trna_r": Arg(0.07, "tRNA radius size"),
+    "forward_cds_r": Arg(0.07, "Forward CDS track radius size"),
+    "reverse_cds_r": Arg(0.07, "Reverse CDS track radius size"),
+    "rrna_r": Arg(0.07, "rRNA track radius size"),
+    "trna_r": Arg(0.07, "tRNA track radius size"),
     "conserved_seq_r": Arg(0.05, "Conserved seq track radius size"),
     "gc_content_r": Arg(0.15, "GC content track radius size"),
     "gc_skew_r": Arg(0.15, "GC skew track radius size"),
@@ -30,3 +32,41 @@ color_args_dict = {
     "gc_skew_p_color": Arg("olive", "GC skew color for positive value"),
     "gc_skew_n_color": Arg("purple", "GC skew color for negative value"),
 }
+
+cog_letter2color = {
+    "J": "#FCCCFC",  # Translation, ribosomal structure and biogenesis
+    "A": "#FCDCFC",  # RNA processing and modification
+    "K": "#FCDCEC",  # Transcription
+    "L": "#FCDCDC",  # Replication, recombination and repair
+    "B": "#FCDCCC",  # Chromatin structure and dynamics
+    "D": "#FCFCDC",  # Cell cycle control, cell division, chromosome partitioning
+    "Y": "#FCFCCC",  # Nuclear structure
+    "V": "#FCFCBC",  # Defense mechanisms
+    "T": "#FCFCAC",  # Signal transduction mechanisms
+    "M": "#ECFCAC",  # Cell wall/membrane/envelope biogenesis
+    "N": "#DCFCAC",  # Cell motility
+    "Z": "#CCFCAC",  # Cytoskeleton
+    "W": "#BCFCAC",  # Extracellular structures
+    "U": "#ACFCAC",  # Intracellular trafficking, secretion, and vesicular transport
+    "O": "#9CFCAC",  # Posttranslational modification, protein turnover, chaperones
+    "X": "#9CFC9C",  # Mobilome: prophages, transposons
+    "C": "#BCFCFC",  # Energy production and conversion
+    "G": "#CCFCFC",  # Carbohydrate transport and metabolism
+    "E": "#DCFCFC",  # Amino acid transport and metabolism
+    "F": "#DCECFC",  # Nucleotide transport and metabolism
+    "H": "#DCDCFC",  # Coenzyme transport and metabolism
+    "I": "#DCCCFC",  # Lipid transport and metabolism
+    "P": "#CCCCFC",  # Inorganic ion transport and metabolism
+    "Q": "#BCCCFC",  # Secondary metabolites biosynthesis, transport and catabolism
+    "R": "#E0E0E0",  # General function prediction only
+    "S": "#CCCCCC",  # Function unknown
+    "-": "#B8B8B8",  # No COG classified
+}
+
+for letter, color in cog_letter2color.items():
+    original_rgb = colors.to_rgb(color)
+    original_hsv = colors.rgb_to_hsv(original_rgb)
+    converted_hsv = [original_hsv[0], original_hsv[1] + 0.5, original_hsv[2]]
+    converted_rgb = colors.hsv_to_rgb(converted_hsv)
+    converted_hex = colors.to_hex(converted_rgb)
+    cog_letter2color[letter] = converted_hex
