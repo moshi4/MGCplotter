9dda4d9956d91f36233c043c807de389d953d1f8
moshi <sy.41211@gmail.com>
2022-04-12 21:16:42 +0900

    Fix: test code to confirm Github Actions error in detail

--- tests/test_mgcplotter.py
+++ tests/test_mgcplotter.py
@@ -12,6 +12,7 @@ def test_minimal_option_run(
     res = sp.run(cmd, shell=True, capture_output=True)
 
     assert res.returncode == 0
+    assert len(list(tmp_path.glob("*.png"))) == 1
     assert (tmp_path / "circos.png").exists()
 
 
